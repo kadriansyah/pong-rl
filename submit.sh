eafb0482d455974a558b4b982db820963cf5a1e1
Kiagus Arief Adriansyah <arief@alodokter.com>
2019-04-23 08:05:15 +0700

    first init

---
+++ submit.sh
@@ -0,0 +1,23 @@
+# Please specify your Google Cloud Storage bucket here
+# GCS_BUCKET="gs://my-bucket/"
+BUCKET=$GCS_BUCKET
+
+TRAINER_PACKAGE_PATH="./trainer"
+MAIN_TRAINER_MODULE="trainer.task"
+
+now=$(date +"%Y%m%d_%H%M%S")
+JOB_NAME="pong_$now"
+
+JOB_DIR=$BUCKET$JOB_NAME
+
+gcloud ml-engine jobs submit training $JOB_NAME \
+    --job-dir $JOB_DIR \
+    --package-path $TRAINER_PACKAGE_PATH \
+    --module-name $MAIN_TRAINER_MODULE \
+    --region us-central1 \
+    --config config.yaml \
+    --runtime-version 1.10 \
+    -- \
+    --output-dir $BUCKET"pong_$now" \
+    --learning-rate 0.0005 \
+    --save-checkpoint-steps 10 \
